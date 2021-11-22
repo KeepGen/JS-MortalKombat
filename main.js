3b7bb5fc108e2d1a3885110190ac2718b0ac5440
Alex <keeper.gen@gmil.com>
2021-11-22 21:04:34 +0200

    Changed player name's order

--- main.js
+++ main.js
@@ -241,7 +241,7 @@ function generateLogs(type, player1, player2, hitValue) {
 			text = `<p>${pattern}</p>`;
 			break
 		case 'hit':
-			pattern = logs['hit'][getRandom(logs['hit'].length-1)].replace('[playerKick]', (player2.name)).replace('[playerDefence]', (player1.name));
+			pattern = logs['hit'][getRandom(logs['hit'].length-1)].replace('[playerKick]', (player1.name)).replace('[playerDefence]', (player2.name));
 			text = `<p>${time} – ${pattern} <span style="color: #ff0000;">-${hitValue}</span> [${player2.hp}/100]</p>`;
 			break
 		case 'defence':
@@ -249,7 +249,7 @@ function generateLogs(type, player1, player2, hitValue) {
 			text = `<p>${time} – ${pattern}</p>`;
 			break
 		case 'end':
-			pattern = logs['end'][getRandom(logs['end'].length-1)].replace('[playerWins]', (player1.name)).replace('[playerLose]', (player2.name));
+			pattern = logs['end'][getRandom(logs['end'].length-1)].replace('[playerWins]', (player2.name)).replace('[playerLose]', (player1.name));
 			text = `<p>${pattern}</p>`;
 			break
 		case 'draw':
